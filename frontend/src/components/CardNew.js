9d6dfb8f1d83a9a298366da185e3bac20712bd04
Suchita Chaudhary <66513629+Suchita01@users.noreply.github.com>
2024-04-09 00:21:35 +0530

    updated card design

--- frontend/src/components/CardNew.js
+++ frontend/src/components/CardNew.js
@@ -4,22 +4,21 @@ import ListGroup from 'react-bootstrap/ListGroup';
 function KitchenSinkExample() {
   return (
     <div className='container'>
-    <Card style={{ width: '18rem',marginTop:'50px' }}>
-      <Card.Img variant="top" src="b1.jpg" style={{height:'280px'}} />
+    <Card style={{ width: '18rem',marginTop:'50px',textAlign:'center' }}>
+      <Card.Img variant="top" src="b1.jpg" style={{height:'250px'}} />
       <Card.Body>
-        <Card.Title>Book Name</Card.Title>
+        <Card.Title>A Suitable Boy</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        A Suitable Boy is a novel by Vikram Seth, published in 1993. With 1,349 pages, the English-language book is one of the longest novels published in a single volume.
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Author Name : Vikram Seth</ListGroup.Item>
        
       </ListGroup>
       <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="#">Download</Card.Link>
+        <Card.Link href="#">More</Card.Link>
       </Card.Body>
     </Card>
     </div>
